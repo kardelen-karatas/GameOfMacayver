2ebb4b68ba454d4f3f445012d2f1a875c358e95b
karde <kardelen.karatas@gmail.com>
2018-10-28 20:17:01 +0100

    palyer movements added

--- game.py
+++ game.py
@@ -9,10 +9,9 @@ Date: 23 october 2018
 class Lab:
 
     def __init__(self, rows, wall):
+        self._player = None
         self._rows = rows
         self._wall = wall
-        # self._moving_object = moving_obect
-        # self._player = player
         self._lab = [
             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
             for _ in range(self._rows)
@@ -23,15 +22,6 @@ class Lab:
 
     def wall(self):
         return self._wall
-
-    # def moving_object(self):
-    #     return self._moving_object
-
-    # def player_x(self):
-    #     return self._player.x()
-
-    # def player_y(self):
-    #     return self._player.y()
 
     def create_lab(self):
 
@@ -47,19 +37,22 @@ class Lab:
 
     def add_player(self, player):
         self._player = player
-        self._lab[player.y()][player.x()] = 'P'
-
+        self._lab[player.y()][player.x()] = player.symbol()
         return self._lab
+
+    def move_player(self, dest_x, dest_y):
+        self._lab[self._player.y()][self._player.x()] = " "
+        self._lab[dest_y][dest_x] = self._player.symbol()
 
     def __str__(self):
         return '\n'.join([repr(l) for l in self._lab])
 
 
 class Player:
-
-    def __init__(self, x, y,lab):
+    def __init__(self, x, y, symbol, lab):
         self._x = x
         self._y = y
+        self._symbol = symbol
         self._lab = lab
 
     def x(self):
@@ -68,3 +61,21 @@ class Player:
     def y(self):
         return self._y
 
+    def symbol(self):
+        return self._symbol
+
+    def move_right(self):
+        self._lab.move_player(self._x + 1, self._y)
+        self._x += 1
+
+    def move_left(self):
+        self._lab.move_player(self._x - 1, self._y)
+        self._x -= 1
+
+    def move_down(self):
+        self._lab.move_player(self._x, self._y + 1)
+        self._y += 1
+
+    def move_up(self):
+        self._lab.move_player(self._x, self._y - 1)
+        self._y -= 1
