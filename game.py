53123fda695222dc2beb6609a621adc7b4fe3d12
karde <kardelen.karatas@gmail.com>
2018-10-28 17:14:17 +0100

    Modification for create_lab() method of Lab class

--- game.py
+++ game.py
@@ -8,12 +8,12 @@ Date: 23 october 2018
 
 class Lab:
 
-    def __init__(self, rows, wall, moving_obect, player):
+    def __init__(self, rows, wall):
         self._rows = rows
         self._wall = wall
-        self._moving_object = moving_obect
-        self._player = player
-        self._lab =  [
+        # self._moving_object = moving_obect
+        # self._player = player
+        self._lab = [
             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
             for _ in range(self._rows)
         ]
@@ -24,31 +24,30 @@ class Lab:
     def wall(self):
         return self._wall
 
-    def moving_object(self):
-        return self._moving_object
+    # def moving_object(self):
+    #     return self._moving_object
 
-    def player_x(self):
-        return self._player.x()
+    # def player_x(self):
+    #     return self._player.x()
 
-    def player_y(self):
-        return self._player.y()
+    # def player_y(self):
+    #     return self._player.y()
 
     def create_lab(self):
 
-        x = self.player_x() - 1
-        y = self.player_y() - 1
-
         for i in range(self._rows):
-            
+
             if i == 0 or i == self._rows - 1:
                 self._lab[i] = [self._wall for _ in self._lab[i]]
             else:
                 self._lab[i][0] = self._wall
                 self._lab[i][-1] = self._wall
-            
-            if i == y:
-                self._lab[i][x] = 'P'
-            
+
+        return self._lab
+
+    def add_player(self, player):
+        self._player = player
+        self._lab[player.y()][player.x()] = 'P'
 
         return self._lab
 
@@ -58,9 +57,10 @@ class Lab:
 
 class Player:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y,lab):
         self._x = x
         self._y = y
+        self._lab = lab
 
     def x(self):
         return self._x
@@ -68,4 +68,3 @@ class Player:
     def y(self):
         return self._y
 
-    
